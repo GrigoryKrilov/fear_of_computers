dc2c03d47d17b4753bc7f2745f09dfa358dc6b38
GrigoryKrilov <grid33@yandex.ru>
2025-10-08 19:12:51 +0300

    что то новое

--- New/zadacha_95.py
+++ New/zadacha_95.py
@@ -1,6 +1,21 @@
-def up_letter (text):
-    result = text[0].upper() + text[1:]
-    return result
+# def up_letter (text):
+#     result = text[0].upper() + text[1:]
+#     return result
 
 
-print(up_letter("привет"))
+# print(up_letter("привет"))
+
+
+
+
+text = "привет.пока"
+result = text[0].upper() + text[1:]
+for i in range(len(text)-1):
+    if result[i] == ".":
+        next_char = result[i+1]
+        next_char_up = result[i+1].upper() + result[i+2:]
+        len_nextcharup = len(next_char)
+        
+        final = result+ next_char_up
+        print(final)
+        

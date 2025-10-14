545d196c1ed1c4b156b3967b7b6611f06fde7174
GrigoryKrilov <grid33@yandex.ru>
2025-10-14 21:54:55 +0300

    сделал из нее функцию

--- New/zadacha_95_1.py
+++ New/zadacha_95_1.py
@@ -1,15 +1,17 @@
-text = "привет.пока"
 
-result = text[0].upper() + text[1:]
+def text_upper(text):
 
+    result = text[0].upper() + text[1:]
 
-for i in range(len(text)-1):
-    
-
-    if result[i] == ".":
-
-        next_char = result[i+1]
-        next_char_up = result[i+1].upper() + result[i+2:]
-        final = result[:7]+ next_char_up
-        print(final)
+    for i in range(len(text)-1):
         
+        if result[i] == ".":
+            len_for_dot = len(result[:i+1])
+            next_char = result[i+1]
+            next_char_up = result[i+1].upper() + result[i+2:]
+            final = result[:len_for_dot]+ next_char_up
+
+            return final
+        
+
+print(text_upper("я ни кто иной.как бэтмен "))

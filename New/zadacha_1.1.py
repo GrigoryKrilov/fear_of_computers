a69da511f9f68396341c74bc6eecd33e8f8aa058
GrigoryKrilov <grid33@yandex.ru>
2026-02-08 23:53:33 +0300

    реализовал загрузку и меню

--- New/zadacha_1.1.py
+++ New/zadacha_1.1.py
@@ -1,33 +1,70 @@
 import json
 grades = {}
 
+
 def add_student(name,marks):
     grades[name]=marks
     return grades
 
-def add_marks(name,new_marks):
+# name_st = input("введите имя:    ")
+# grades_st = list(input("введите оценки: "))
+# add_student(name_st,grades_st)
+
+def add_marks():
+    name = input("Введите имя:   ")
+    new_marks = list(input("Введите оценки:   "))
     if name in grades:
         grades[name].extend(new_marks)
+        print("Оценки обновленны")
+        
+# add_marks()
 
 def save_file(filename):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(grades, file, ensure_ascii=False, indent=4)
     print("Данные успешно сохранены")
 
+# save_file("test01")
+    
 def load_file(filename):
      with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            return data
+            global grades
+            grades = json.load(file)
+            print("Данные успешно загруженны")
+            return grades
   
+# load_file("test01")
+# print(grades)
 
-# name_st = input("введите имя:    ")
-# grades_st = list(input("введите оценки: "))
-# add_student(name_st,grades_st)
 
-# grades_1 = list(input("введите оценки: "))
+# load_file("test01")
+# print(grades)
 
+# add_marks()
+# print(grades)
 
 # save_file("test01")
-zagruzka = load_file("test01")
-print(zagruzka)
+
+
+def menu():
+     while True:
+        print("1 Загрузить")
+        print("2 Добавить")
+        print("3 Сохранить")
+        print("4 Выход")
+        
+        choice= input("Ввод:  ")
+
+        if choice == "1":
+            name = input("введите имя файаладля загрузки:  ")
+            load_file(name)
+        elif choice == "2":
+            add_marks()
+        elif choice == "3":
+            save =input("Введите имя для сохранения:    ")
+            save_file(save)
+        elif choice == "4":
+            break
+
+menu()
 # print(grades)

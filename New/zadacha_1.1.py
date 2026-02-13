5cc8478dc6edeb4a133cc3a811d04b1c9c76abd9
GrigoryKrilov <grid33@yandex.ru>
2026-02-13 15:45:43 +0300

    добавли функцию удаление

--- New/zadacha_1.1.py
+++ New/zadacha_1.1.py
@@ -6,9 +6,6 @@ def add_student(name,marks):
     grades[name]=marks
     return grades
 
-# name_st = input("введите имя:    ")
-# grades_st = list(input("введите оценки: "))
-# add_student(name_st,grades_st)
 
 def add_marks():
     name = input("Введите имя:   ")
@@ -16,55 +13,55 @@ def add_marks():
     if name in grades:
         grades[name].extend(new_marks)
         print("Оценки обновленны")
+        return print(grades)
         
-# add_marks()
-
 def save_file(filename):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(grades, file, ensure_ascii=False, indent=4)
     print("Данные успешно сохранены")
-
-# save_file("test01")
+    return print(grades)
     
 def load_file(filename):
      with open(filename, 'r', encoding='utf-8') as file:
             global grades
             grades = json.load(file)
             print("Данные успешно загруженны")
-            return grades
-  
-# load_file("test01")
-# print(grades)
-
-
-# load_file("test01")
-# print(grades)
-
-# add_marks()
-# print(grades)
-
-# save_file("test01")
-
+            return print(grades)
+     
+def del_stud():
+    name_del = input("введите Имя которое хотите удалить:    ")
+    del grades[name_del]
+    print("удаление",name_del,"успешно")
+    return print(grades)
 
 def menu():
      while True:
-        print("1 Загрузить")
-        print("2 Добавить")
-        print("3 Сохранить")
-        print("4 Выход")
+        print("1 Создать нового")
+        print("2 Загрузить")
+        print("3 Добавить")
+        print("4 Сохранить")
+        print("5 Удалить")
+        print("6 Выход")
         
         choice= input("Ввод:  ")
+        if choice =="1":
+            name_1= input("Введите имя нового студента: ")
+            marks = list(input("введите его оценки:        "))
+            add_student(name_1,marks)
 
-        if choice == "1":
+        elif choice == "2":
             name = input("введите имя файаладля загрузки:  ")
             load_file(name)
-        elif choice == "2":
-            add_marks()
+            print(grades)
         elif choice == "3":
+            add_marks()
+        elif choice == "4":
             save =input("Введите имя для сохранения:    ")
             save_file(save)
-        elif choice == "4":
+        elif choice =="5":
+            del_stud()
+        elif choice == "6":
+            print(grades)
             break
 
 menu()
-# print(grades)

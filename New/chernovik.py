c605eb6cdb38ea0b26cb7c689209654d6330c872
GrigoryKrilov <grid33@yandex.ru>
2026-06-01 18:06:53 +0300

    изучаю классы

--- New/chernovik.py
+++ New/chernovik.py
@@ -1,10 +1,26 @@
-def decorator_simp (func):
-    def wrapper():
-        print("0")
+class Treebonsay:
+    def __init__(self,name,height):
+        self.name=name
+        self.height=height
+        print("дерево создано","название",name,"Рост",height)
         
-        print("2")
-    return wrapper
-@ decorator_simp
-def education():
-    print("1")
-education()
+
+new_tree= Treebonsay("elm",10)
+new_tree_1= Treebonsay("maple",5)
+
+
+
+# class bugs:
+#     def __init__(self,name,hazard):
+        
+
+
+
+
+# juniper = tree_bonsay()
+# maple = tree_bonsay()
+
+# aphid = bugs()
+# scale = bugs()
+# mite = bugs()
+

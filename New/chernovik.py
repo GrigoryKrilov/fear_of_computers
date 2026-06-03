99049b5ccece02ed9f82deb76e5f72343417c602
GrigoryKrilov <grid33@yandex.ru>
2026-06-03 16:44:34 +0300

    описал классы в знаниях

--- New/chernovik.py
+++ New/chernovik.py
@@ -1,13 +1,15 @@
-class Treebonsay:
-    def __init__(self,name,height):
+class Treebonsay: # создаем класс, называем его Treebonsay с заглавной буквы без нижнего прочерка.
+    def __init__(self,name,height): # инит значит определить / создать экземпляр с параметрами  name и heitht имя и рост соотвесвенно.
         self.name=name
         self.height=height
         print("дерево создано","название",name,"Рост",height)
-        
+# грубо говоря это тело  "функции"
 
+
+# создаем новые эксемляры с названием и парамнтром роста и сохранеям в переменную.
 new_tree= Treebonsay("elm",10)
-new_tree_1= Treebonsay("maple",5)
-
+new_tree_1= Treebonsay("maple",5)  
+# тут мы как бы вызваем эту  "функцию"
 
 
 # class bugs:

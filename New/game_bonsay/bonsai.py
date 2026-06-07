48e9caaae922c502913e97327d6184df8f3c0349
GrigoryKrilov <grid33@yandex.ru>
2026-06-07 23:03:19 +0300

    работаю над игрой

--- New/game_bonsay/bonsai.py
+++ New/game_bonsay/bonsai.py
@@ -1,16 +1,33 @@
 import pygame
-print(pygame.ver)
+import sys  # библетека для создания скриптов.
+# print(pygame.ver)
+pygame.init()
+# высота и ширинка экрана
+WIDTH,HEIGHT = 800,600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Бонсай-релаксация") # надпись
+# цвета в РГБ
+BACKGROUND_COLOR = (144, 238, 144)  # зеленый
+BONSAI_COLOR = (139, 69, 19)  # коричневый
+# время
+clock = pygame.time.Clock()
+FPS = 60
 
+runnung = True
+while runnung:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runnung =False
+            
+    screen.fill(BACKGROUND_COLOR)
+    pygame.display.flip()
+    clock.tick(FPS)
+ 
+pygame.quit()
+sys.tick(FPS)
 
-
-
-# python -m pip install pygame
-
-# which python
-
-# python -c "import sys; print(sys.version)"
-
+# новое перенести в тетрадку.
 
 # py --version
-
+# which python
 # py -m pip install pygame
